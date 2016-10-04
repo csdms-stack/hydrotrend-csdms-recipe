62437399b6e8433b56c3a0dc367af26a4466ae5c
mcflugen <mcflugen@gmail.com>
2016-10-04 10:05:20 -0600

    Use print function from the future.

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -8,5 +8,5 @@ from pymt.components import Hydrotrend
 
 ht = Hydrotrend()
 for default in ht.defaults:
-    print '{name}: {val} {units}'.format(
-        name=default[0], val=default[1], units=val[2])
+    print('{name}: {val} {units}'.format(
+        name=default[0], val=default[1], units=val[2]))
