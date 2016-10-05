85b0a9eb31b3aeccf64be11b427cbe2162fe2905
mcflugen <mcflugen@gmail.com>
2016-10-04 18:12:51 -0600

    Added debug statements.

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -4,9 +4,35 @@ import os
 os.mkdir('_testing')
 os.chdir('_testing')
 
-from pymt.components import Hydrotrend
+from pymt.babel import setup_babel_environ
 
-ht = Hydrotrend()
+setup_babel_environ()
+print(os.environ)
+
+try:
+    print('import csdms')
+    import csdms
+except ImportError:
+    raise
+else:
+    print(csdms.__path__)
+
+try:
+    print('import csdms.Hydrotrend')
+    import csdms.Hydrotrend
+except ImportError:
+    raise
+else:
+    print(Hydrotrend())
+
+try:
+    print('from pymt.components import Hydrotrend')
+    from pymt.components import Hydrotrend
+except ImportError:
+    raise
+else:
+    ht = Hydrotrend()
+
 for default in ht.defaults:
     print('{name}: {val} {units}'.format(
         name=default[0], val=default[1], units=val[2]))
