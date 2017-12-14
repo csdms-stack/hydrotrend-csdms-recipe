6005eeb5a1ea42c4323dea122487342cb32c234e
Mark Piper <mark.piper@colorado.edu>
2017-12-13 17:07:54 -0700

    Include IRF steps in run test
    I've noted that the babelized Hydrotrend can be instantiated, yet fail
    at initialize or update.

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -11,3 +11,8 @@ model = Model()
 for default in model.defaults:
     print('{name}: {val} {units}'.format(
         name=default[0], val=default[1][0], units=default[1][1]))
+
+config_file, initdir = model.setup(os.getcwd())
+model.initialize(config_file, dir=initdir)
+model.update()
+model.finalize()
